e0f08f4c19903832aa81c51b65e3335ac06b6bd2
Joao Gabriel <gois.jg.dev@gmail.com>
2020-06-24 01:11:24 -0300

    Correlation of routes,models,repos & services done

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -2,29 +2,38 @@ import { Router } from 'express';
 import { startOfHour, parseISO } from 'date-fns';
 
 import AppointmentRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentRouter = Router();
 const appointmentsRepository = new AppointmentRepository();
 
 
-appointmentRouter.post('/', (request, response) => {
-   const { provider, date } = request.body;
+appointmentRouter.get('/', (request, response) =>{
+   const appointments = appointmentsRepository.findAll();
 
-   const parsedDate = startOfHour(parseISO(date));
-   
-   const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
-
-   if (findAppointmentInSameDate) {
-      return response
-         .status(400)
-         .json({
-            message: 'This appointment is already booked' 
-         });
-   }
-
-   const appointment = appointmentsRepository.create(provider, parsedDate);
-
-   return response.json(appointment);
+   return response.json(appointments);
 })
+
+appointmentRouter.post('/', (request, response) => {
+   try {
+      const { provider, date } = request.body;
+
+      const parsedDate = parseISO(date);
+
+      const createAppointmentService = new CreateAppointmentService(appointmentsRepository);
+
+      const appointment = createAppointmentService.execute({
+         provider,
+         date: parsedDate
+      });
+      
+      return response.json(appointment);
+
+   } catch(err) {
+      return response.status(400).json({
+         error: err.message
+      });
+   }
+});
 
 export default appointmentRouter;
