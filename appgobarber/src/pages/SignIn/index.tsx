1892c842f3e1f61887b2dfa28ccd1d61dac02d42
Joao Gabriel <gois.jg.dev@gmail.com>
2020-07-28 23:49:39 -0300

    APP_SignIn screen done

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -1,25 +1,52 @@
 import React from 'react';
-import { Image, Alert } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, Alert } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logo from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logo} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex:1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <Container>
+          <Image source={logo} />
 
-      <Title>Faça seu login</Title>
+          <Title>Faça seu login</Title>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button onPress={() => {Alert.alert(`On press launching!`)}}>Entrar</Button>
-    </Container>
+          <Button onPress={() => {Alert.alert(`Sign In Button Pressed!`)}}>Entrar</Button>
+
+          <ForgotPassword onPress={() => {Alert.alert('Forgot Password Button Pressed!')}}>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => {
+        Alert.alert('Create Account Button Pressed!');
+      }}>
+        <Icon name="log-in" size={20} color="#FF9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   )
 };
 
--- appgobarber/src/pages/SignIn/styles.ts
+++ appgobarber/src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 const Container = styled.View`
   flex:1;
@@ -14,4 +15,42 @@ const Title = styled.Text`
   margin: 64px 0 24px;
 `;
 
-export { Container, Title };
+const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+const ForgotPasswordText = styled.Text`
+  color: #F4EDE8;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+const CreateAccountButton = styled.TouchableOpacity`
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #312E38;
+  border-top-width: 1px;
+  border-color: #322129;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const CreateAccountButtonText = styled.Text`
+  color: #FF9000;
+  font-size: 18px;
+  font-family: 'RobotoSlabe-Regular'
+`;
+
+export {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText
+};
